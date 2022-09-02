80fe10b8e83e47ecd566ea935264d2ed4597f380
Kesler Mathieu <kmat5@icloud.com>
2022-09-01 20:08:11 -0400

    temporary ssubmit

--- pset0/wc61.cc
+++ pset0/wc61.cc
@@ -1,30 +1,24 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cctype>
+#include <string>
+#include <iostream>
+#include <cstring>
 
 using namespace std;
+const char* NEWLINE = "\n";
 
-int cout_lines(FILE *std_in);
-int cout_words(FILE *std_in);
-int cout_bytes(FILE *std_in);
-
-int main() {
-    //TODO: given that each function call will move stdin file pointer to EOF, we need to call rewind() twice because of this modular design choice
-    // For performance benefits, we can perform counting by one pass through of stdin!!
-    int line_cout = cout_lines(stdin);
-    int word_cout = cout_words(stdin);
-    int byte_cout = cout_bytes(stdin);
-
+int main() {    
+    int line_cout = 0, word_cout = 0, byte_cout = 0;
+    char* buffer = (char*) calloc(1, sizeof(char));
+    while(fread(buffer, sizeof(char), 1 , stdin) == 1){
+        byte_cout++;
+        //TODO: to get word count, we can iterate 
+        if(strcmp(buffer, NEWLINE) == 0){
+            line_cout++;
+        }
+    }
+    free(buffer);
+    fprintf(stdout, "       %i       %i       %i\n", line_cout, word_cout, byte_cout);
     exit(0);
-}
-
-int cout_lines(FILE *std_in){
-
-}
-
-int cout_words(FILE *std_in){
-    
-}
-
-int cout_bytes(FILE *std_in){
-    
 }
