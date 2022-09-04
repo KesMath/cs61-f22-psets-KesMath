33ec21c14c9edbea33add274aade75ee1c275b50
Kesler Mathieu <kmat5@icloud.com>
2022-09-04 00:35:18 -0400

    edge cases for strstr()

--- pset0/strstr61.cc
+++ pset0/strstr61.cc
@@ -2,9 +2,24 @@
 #include <cassert>
 #include <cstdio>
 
+//TODO: replace strlen() lib function with custom version given that this exercise is suppose to simulate a lib call that has no ref to other lib
 char* mystrstr(const char* s1, const char* s2) {
-    // Your code here
+    // If s2 or needle points to a string with zero length, the function shall return s1.
+    if(strlen(s2) == 0){
+        return (char*) &s1[0]; //not sure if "const_cast" is safer practice
+    }
+
+    // Case when haystack is empty
+    if(strlen(s1) == 0){
+        return nullptr;
+    }
+    // Case when strlen(needle) > strlen(haystack) (implying the needle is a size of a haystack and vice-versa)
+    if(strlen(s2) > strlen(s1)){
+        return nullptr;
+    }
+    //PSEUDO for CHECKING FIRST OCCURRENCE:
     return nullptr;
+
 }
 
 int main(int argc, char* argv[]) {
